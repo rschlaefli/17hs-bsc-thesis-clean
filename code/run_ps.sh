42c069ec10f084d7640a428eb2f635d757c98bd0
Roland Schläfli <rolandschlaefli@gmail.com>
2018-01-16 10:58:04 +0100

    Prepare exp. 23

--- code/run_ps.sh
+++ code/run_ps.sh
@@ -15,19 +15,16 @@ source ~/anaconda3/bin/activate gru-minimal
 echo "> Running configs..."
 
 # go through all possible configs
-# for i in {0..2}
-for i in {20..22}
+for i in {23..23}
 do
   # ensure the log dir for this version is existent
   mkdir -p $LOG_DIR
 
   # run the experiment, redirecting all output to the logfiles
   ~/anaconda3/envs/gru-minimal/bin/python ./02_MODELLING/LSTM_$1.py $i $PATIENCE $MODEL_VERSION $EVALUATE >> $LOG_DIR/$MODEL-$MODEL_VERSION_$PATIENCE_$i.txt
-
-  # TODO: any cleanup?
 done
 
 echo "> Finished running configs. Shutdown..."
 
 # shutdown the machine after completion of all experiments
-sudo shutdown
+# sudo shutdown
